55453c2853390d182d3ace8fe7cedaf403d673af
Katie Bridson <kbridson@memphis.edu>
2024-04-03 04:10:34 -0500

    Added run2 test - skipped, need to make Game#action in order to change player and game pot based on user bet amount

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,7 @@ require 'game'
 
 RSpec.describe Game do
   describe '#play' do
-    it 'plays a 2 player game with raise then see, no folds, no discards' do
+    it 'plays a 2 player game (run0) with raise then see, no folds, no discards' do
       game = Game.new
 
       p1 = Player.new
@@ -36,7 +36,7 @@ RSpec.describe Game do
       expect($stdin).to have_received(:gets).exactly(6).times
     end
 
-    it 'plays a different 2 player game with raise then see, no folds, no discards' do
+    it 'plays a 2 player game (run1) with raise then see, no folds, no discards' do
       game = Game.new
 
       p1 = Player.new
@@ -65,6 +65,38 @@ RSpec.describe Game do
       game.play
 
       expect($stdout.string.chomp).to eq(File.read('spec/fixtures/run1.txt'))
+      expect($stdin).to have_received(:gets).exactly(6).times
+    end
+
+    xit 'plays a 2 player game (run2) with raise then see, no folds, no discards' do
+      game = Game.new
+
+      p1 = Player.new
+      p1.pot = 100
+      p2 = Player.new
+      p2.pot = 100
+      hand1 = '9♠, 10♠, J♦, Q♠, K♠'
+      hand2 = '2♣, 4♣, 9♦, J♣, K♦'
+      allow(p1).to receive(:hand).and_return hand1
+      allow(p2).to receive(:hand).and_return hand2
+      game.players = [p1, p2]
+
+      allow(Hand).to receive(:strength).with(hand1).and_return('Flush')
+      allow(Hand).to receive(:strength).with(hand2).and_return('High Card')
+
+      $stdout = StringIO.new
+      inputs = ['raise 5', 'call', 'none', 'none', 'raise 10', 'call']
+      input_enum = inputs.to_enum
+
+      allow($stdin).to receive(:gets) do
+        input = input_enum.next
+        $stdout.print("#{input}\n")
+        input
+      end
+
+      game.play
+
+      expect($stdout.string.chomp).to eq(File.read('spec/fixtures/run2.txt'))
       expect($stdin).to have_received(:gets).exactly(6).times
     end
   end
